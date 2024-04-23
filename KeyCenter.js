97e2b783c8bc718af869d2d941fabd2e0f8e7b23
Favaz <favazpathiyassery07@gmail.com>
2024-04-23 11:24:13 +0530

    WIP

--- KeyCenter.js
+++ KeyCenter.js
@@ -1,7 +1,7 @@
 //storing keys here because we are not publishing the app
 const KeyCenter = {
-  appID: 906329135,
-  appSign: '0d24039375d1752a8503e748e873afe34311ece909db38d6c7a3e26a915a0cd9',
+  appID: 123,
+  appSign: '1234',
 };
 
 export default KeyCenter;
